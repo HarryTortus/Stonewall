84d4cf67d98890b4f106ca68fae6f7d44fc2944d
HarryTortus <mickeywilliamsonart@gmail.com>
2026-08-29 23:46:28 -0400

    Test Aspect Keep

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788059736|8423041004';
+const CACHE_VERSION = '1788061541|10227787967';
 /** @type {string} */
 const CACHE_PREFIX = 'Stonewall-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
