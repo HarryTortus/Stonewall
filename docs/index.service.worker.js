8118621a1e7f85f5dfa1c578f6f301897b21819d
HarryTortus <mickeywilliamsonart@gmail.com>
2026-08-29 23:16:14 -0400

    Update for web

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788053391|2077676883';
+const CACHE_VERSION = '1788059736|8423041004';
 /** @type {string} */
 const CACHE_PREFIX = 'Stonewall-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
